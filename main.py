5837a773c230701c760be8c856d4987222b69f1d
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2022-10-02 12:51:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from cv2 import cv2
 def extracting_faces(img_path):
     count = 0
     faces = face_recognition.load_image_file(img_path)
-    #print(faces) хз что выведет
+    # print(faces) хз что выведет
     faces_locations = face_recognition.face_locations(faces)
 
     for face_location in faces_locations:
@@ -19,16 +19,15 @@ def extracting_faces(img_path):
         count += 1
 
 
-def find_in_base(img_path, base_path): 
+def find_in_base(img_path, base_path):
     img = face_recognition.load_image_file(img_path)
     img_encodings = face_recognition.face_encodings(img)[0]
-    base = # хз
+    base =  # хз
     for base_item in base:
-            base_item_encodings = face_recognition.face_encodings(base_item[0])[0]
-            if compare_faces
-    
-    
-    
+        base_item_encodings = face_recognition.face_encodings(base_item[0])[0]
+        if compare_faces
+
+
 def compare_faces(img1_path, img2_path):
     img1 = face_recognition.load_image_file(img1_path)
     img1_encodings = face_recognition.face_encodings(img1)[0]
@@ -49,9 +48,10 @@ def compare_faces(img1_path, img2_path):
 def main():
     img_path1 = input()
     ing_path2 = input()
-    #extracting_faces(path)
-    compare_faces(img_path1, img_path2)
+    extracting_faces(img_path1)
+    # compare_faces(img_path1, img_path2)
 
 
 if __name__ == '__main__':
     main()
+
